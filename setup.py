97edebe9d56b7a2206043368783f51ed3824f3ad
Felix Leblanc <felix.leblanc1305@gmail.com>
2016-01-14 19:02:01 -0500

    feat(pkg): python package setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='lxclite',
+        version='1.0',
+        description='LXC Lite',
+        author='Élie Deloumeau'
+        author_email='elie@deloumeau.fr',
+        url='https://github.com/googley/LXCLite',
+        packages=['lxclite'],
+)
