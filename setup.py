9ede38b0037f98321e68d163e7c1b9b5325284d9
Felix Leblanc <felix.leblanc1305@gmail.com>
2016-01-14 19:36:33 -0500

    fix(setup): fix setup.py
    utf-8 encoding
    comma after author_email

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
+# coding=utf-8
 from distutils.core import setup
 
 setup(name='lxclite',
         version='1.0',
         description='LXC Lite',
-        author='Élie Deloumeau'
+        author='Élie Deloumeau',
         author_email='elie@deloumeau.fr',
         url='https://github.com/googley/LXCLite',
         packages=['lxclite'],
